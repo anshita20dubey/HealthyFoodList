40a3e3b4b6fec4e490778550ea0416290674f29a
anshita20dubey <anshita20dubey@gmail.com>
2024-09-05 16:31:11 +0530

    Learning fragments and conditional rendering

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,17 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 
 const App = () => {
   const foodItems = ['dal', 'ghee', 'roti', 'green vegetables', 'salad', 'milk'];
+  // const foodItems = [];
+  // if(foodItems.length === 0) {
+  //   return <h3>I'm still hungry</h3>
+  // }
+  // const emptyMessage = foodItems.length === 0 ? <h3>I'm still hungry</h3> : null
   return (
     <>
       <h1>Healthy Food List</h1>
+      {/* {foodItems.length === 0 ? <h3>I'm still hungry</h3> : null} */}
+      {/* {emptyMessage} */}
+      {foodItems.length === 0 && <h3>I'm still hungry</h3>}
       <ul className="list-group">
         {foodItems.map((item)=>(
           <li key={item} className="list-group-item">{item}</li>
